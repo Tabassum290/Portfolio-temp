36cad4c4e42903536017e764814149f16e7b5b66
Tabassum290 <keyatabassum2@gmail.com>
2025-11-12 13:46:41 +0600

    Error solved

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import {
-  createBrowserRouter,
+  createHashRouter,
   RouterProvider,
 } from "react-router-dom";
-import './index.css'
-import App from './App.jsx'
+import './index.css';
+import App from './App.jsx';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
-    element:<App/>,
+    element: <App />,
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
